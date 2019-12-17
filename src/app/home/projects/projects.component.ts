0c6ae25ff5f8ddf0ec0f9e7da2be87afdc659ac3
Charles Barnes <charles@charlesabarnes.com>
2019-12-17 16:05:23 -0600

    add images to project boxes

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[];
+  public loading: boolean = true;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit() {
+    this.getProjects();
+  }
+    
+  private getProjects(): void {
+    this.http.get('assets/projects.json').subscribe((projects: Project[]) => {
+      this.projects = projects;
+      this.loading = false;
+    });
   }
 
+}
+
+export interface Project {
+    name: string;
+    url: string;
+    image: string;
+    tools: string[];
+    role: string[],
+    year: string;
+    description: string;
 }
